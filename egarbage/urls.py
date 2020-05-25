adb38371ac348cbd00147a68a28bf4a4039c4960
Christian <nduxrs@gmail.com>
2020-05-25 20:12:37 +0200

    modified the login page.

--- egarbage/urls.py
+++ egarbage/urls.py
@@ -4,10 +4,10 @@ from egarbage import views
 
 # The application urls in the /hello directory to be renamed in coming versions.
 urlpatterns = [
-    path("", views.home, name='home'),
-    path("home/", views.home, name='home'),
+    path("", views.home, name='about'),
+    path("home/", views.home, name='login'),
     path("about/", views.about, name='about'),
     path("history/", views.history, name='history'),
-    path("register/", views.register, name='history'),
+    path("register/", views.register, name='register'),
 
 ]
