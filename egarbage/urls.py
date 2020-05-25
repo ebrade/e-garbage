92e32a53597b84f8a44c5b8769cade2e82fded1c
Christian <nduxrs@gmail.com>
2020-05-25 21:25:24 +0200

    Account login fixed.

--- egarbage/urls.py
+++ egarbage/urls.py
@@ -5,7 +5,6 @@ from egarbage import views
 # The application urls in the /hello directory to be renamed in coming versions.
 urlpatterns = [
     path("", views.about, name='about'),
-    path("home/", views.home, name='login'),
     path("about/", views.about, name='about'),
     path("history/", views.history, name='history'),
     path("register/", views.register, name='register'),
