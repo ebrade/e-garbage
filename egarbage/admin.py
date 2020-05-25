9fef2575306df77483e9189da0b1dc8ffffd38c7
Christian <nduxrs@gmail.com>
2020-05-25 21:01:04 +0200

    Completed the admin panel.

--- egarbage/admin.py
+++ egarbage/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from egarbage.models import Register
 
 # Register your models here.
+
+
+admin.site.site_title = " E-garbage Admin Panel"
+admin.site.site_header = "E-garbage Admin Panel"
+admin.site.index_title = ""
+
+
+class RegisterAdmin(admin.ModelAdmin):
+    list_display = ['name',
+                    'province',
+                    'district',
+                    'sector',
+                    'cell',
+                    'village',
+                    'street',
+                    'e_waste_type',
+                    'quantity',
+                    'collected',
+                    'timestamp']
+
+
+admin.site.register(Register, RegisterAdmin)
