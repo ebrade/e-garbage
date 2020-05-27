b826aad2ead3a4d82682bfb051dfd2961003855e
Christian <nduxrs@gmail.com>
2020-05-27 18:54:16 +0200

    Add all necessary urls.

--- egarbage/urls.py
+++ egarbage/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('add/', views.RegisterItem.as_view(), name='add_new_item'),
     path('load_district', views.load_district, name='load_district'),
+    path('load_sector', views.load_sector, name='load_sector'),
+    path('load_cell', views.load_cell, name='load_cell'),
+    path('load_village', views.load_village, name='load_village'),
+
 ]
